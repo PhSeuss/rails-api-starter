f75c7dc49e74dc3b9f26b3fdba8504739694bfa5
Thuong <truonghoaithuong2000@gmail.com>
2018-05-28 21:33:50 +0700

    refactor code

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -9,12 +9,10 @@ class V1::ApiController < ApplicationController
   end
 
   def authenticate_token
-    return (
       (token = request.headers['Authorization']) &&
       (token = token.split(' ').last) &&
       (payload = JWT.decode(token, Rails.application.secrets.secret_key_base, { algorithm: 'HS256'}).first) &&
       (user = User.find(payload['data']['user_id'])) &&
       (user.jwt_matcher == payload['data']['jwt_matcher'])
-    )
   end
 end
